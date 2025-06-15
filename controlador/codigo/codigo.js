c26a1260b75a65a928b6efe82f26427d504213b9
maison7 <misaelluna152@gmail.com>
2025-06-14 21:39:49 -0300

    Add files via upload

---
+++ controlador/codigo/codigo.js
@@ -0,0 +1,9 @@
+alert("Bienvenido/a!")
+
+
+
+let nombre;
+nombre=prompt("Ingrese su nombre: ")
+alert("Espero que disfrutes de la pagina personalizada de Enzo, gracias "+nombre)
+ 
+alert("Atte: Maison.")
