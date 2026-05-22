97de15662aa22fb26d5a8f9afcc6dedcbe7ca47d
maison7 <misaelluna152@gmail.com>
2026-05-22 00:28:18 -0300

    Update codigo.js

--- controlador/codigo/codigo.js
+++ controlador/codigo/codigo.js
@@ -1,9 +1,1 @@
-alert("Bienvenido/a!")
 
-
-
-let nombre;
-nombre=prompt("Ingrese su nombre: ")
-alert("Espero que disfrutes de la pagina personalizada de Enzo, gracias "+nombre)
- 
-alert("Atte: Maison.")
